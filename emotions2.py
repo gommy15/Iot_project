4bc6faa606ec2c2935a536033d3cb32a2ddcc0a3
gommy15 <dlgmlfo0415@naver.com>
2023-06-15 21:00:07 +0900

    [ADD] change model tflite

--- emotions2.py
+++ emotions2.py
@@ -1,53 +1,17 @@
-import cv2
-import numpy as np
-from keras.models import load_model
-from utils.datasets import get_labels
-from utils.inference import apply_offsets
-from utils.preprocessor import preprocess_input
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-emotion_model_path = './models/emotion_model.hdf5'
-emotion_labels = get_labels('fer2013')
+# Load the HDF5 model
+hdf5_model_path = './models/_mini_XCEPTION.102-0.66.hdf5'
+loaded_model = load_model(hdf5_model_path)
 
-face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
-emotion_classifier = load_model(emotion_model_path)
-emotion_target_size = emotion_classifier.input_shape[1:3]
+# Convert the loaded model to TFLite format
+converter = tf.lite.TFLiteConverter.from_keras_model(loaded_model)
+tflite_model = converter.convert()
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 320)
-cap.set(4, 240)
+# Save the TFLite model to a file
+tflite_model_path = './models/emotion_model.tflite'
+with open(tflite_model_path, 'wb') as f:
+    f.write(tflite_model)
 
-while True:  # True:
-    ret, bgr_image = cap.read()
-    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
-                                          flags=cv2.CASCADE_SCALE_IMAGE)
-
-    for face_coordinates in faces:
-        x1, x2, y1, y2 = apply_offsets(face_coordinates, (20, 40))
-        gray_face = gray_image[y1:y2, x1:x2]
-        try:
-            gray_face = cv2.resize(gray_face, (emotion_target_size))
-            gray_face = preprocess_input(gray_face, True)
-            gray_face = np.expand_dims(gray_face, 0)
-            gray_face = np.expand_dims(gray_face, -1)
-
-            face_list = emotion_classifier.predict(gray_face)[0]
-            face_index = 0;
-            face_text = emotion_labels[0]
-
-            for i, v in enumerate(face_list):
-                if face_list[i] > face_list[face_index]:
-                    face_index = i
-                    face_text = emotion_labels[i]
-
-            cv2.rectangle(gray_image, (x1, y1), (x2, y2), (0, 0, 255), 3)
-            cv2.putText(gray_image, face_text, (x1, y1), 0, 1, (0, 0, 255), 2)
-        except:
-            continue
-
-    cv2.imshow('video', gray_image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+print("TFLite model saved successfully!")
---
+++ emotions_tflite.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from utils.datasets import get_labels
+from utils.inference import apply_offsets
+import socket
+
+SERVER_PORT = 8888
+
+# 소켓 생성
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', SERVER_PORT))
+server_socket.listen(1)
+
+# 모델 대기
+emotion_model_path = './models/emotion_model.tflite'
+emotion_labels = get_labels('fer2013')
+
+face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
+interpreter = tf.lite.Interpreter(model_path=emotion_model_path)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+emotion_target_size = input_details[0]['shape'][1:3]
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 320)
+cap.set(4, 240)
+
+# 클라이언트 연결 대기
+client_socket, addr = server_socket.accept()
+print('Connected by', addr)
+
+while True:
+    ret, bgr_image = cap.read()
+    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
+                                          flags=cv2.CASCADE_SCALE_IMAGE)
+
+    for face_coordinates in faces:
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, (20, 40))
+        gray_face = gray_image[y1:y2, x1:x2]
+        try:
+            gray_face = cv2.resize(gray_face, emotion_target_size)
+            gray_face = np.expand_dims(gray_face, -1)
+            gray_face = np.expand_dims(gray_face, 0)
+            gray_face = gray_face.astype(np.float32) / 255.0
+
+            interpreter.set_tensor(input_details[0]['index'], gray_face)
+            interpreter.invoke()
+            predictions = interpreter.get_tensor(output_details[0]['index'])
+            face_index = np.argmax(predictions[0])
+            face_text = emotion_labels[face_index]
+
+            cv2.rectangle(gray_image, (x1, y1), (x2, y2), (0, 0, 255), 3)
+            cv2.putText(gray_image, face_text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+            client_socket.sendall(face_index.encode())
+        except:
+            continue
+
+    cv2.imshow('video', gray_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # 소켓 닫기
+        client_socket.close()
+        server_socket.close()
+        break
+
+cap.release()
+cv2.destroyAllWindows()
